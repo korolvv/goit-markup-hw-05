c287f846a79a47ac13175358cef54298d3cf97a2
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-10-21 17:04:11 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,13 @@ window.addEventListener("DOMContentLoaded", () => {
 		refs.openModalBtn.addEventListener("click", toggleModal);
 		refs.closeModalBtn.addEventListener("click", toggleModal);
 
+		refs.closeModalBtn.addEventListener("click", () => {
+			refs.modal.classList.add("closed-style");
+			setTimeout(function () {
+				refs.modal.classList.remove("closed-style");
+			}, 2000);
+		});
+
 		function toggleModal() {
 			refs.modal.classList.toggle("is-hidden");
 			refs.body.classList.toggle("is-visible");
